8b5b09bd622d77433771057c5db6a7b49fb13586
shuvam Sengupta <36786690+Shuvam588243@users.noreply.github.com>
2021-08-02 17:10:47 +0000

    Front

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 
 //fetching the container where card will be added
 const taskContainer = document.querySelector('.task_container');
+//Initializing a Empty Global store array
 const globalStore = [];
 
+
+const generateNewCard = (taskData) =>
+  `
+  <div class="col-sm-12 col-md-6 col-lg-4 h-100 mb-4" id=${taskData.id} style="height:200px">
+    <div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success"><i class="fas fa-edit"></i></button>
+    <button type="button" class="btn btn-outline-danger" onclick="taskDelete()"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="card-body">
+      <img src=${taskData.imageUrl} class="card-img-top" alt="event image">
+    <h5 class="card-title fw-bolder text-primary mt-3">${taskData.taskTitle}</h5><span class="badge bg-secondary p-1">${taskData.taskType}</span>
+    <p class="card-text">${taskData.taskDescription}</p>
+    <a href="#" class="btn btn-primary px-4">Open</a>
+  </div>
+  </div>
+  `;
+
+
+
+//Loading Initial Data
 const loadInitialCardData = () =>
 {
   //Localstorage to access Card Data
@@ -12,7 +34,9 @@ const loadInitialCardData = () =>
   //loop over the array of task objects to create HTML Cards, inject it in DOM
   cards.map((card)=>
   {
-    taskContainer.insertAdjacentHTML('beforeend', newCard);
+    taskContainer.insertAdjacentHTML('beforeend', generateNewCard(card));
+    //Updates Global store
+    globalStore.push(card);
   });
 }
 
@@ -27,9 +51,9 @@ const saveChanges = () =>
     taskDescription : document.getElementById('description').value,
   }
   
-  //Making the New Cart
+  //Making the New Card
   const newCard = `
-  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}">
+  <div class="col-sm-12 col-md-6 col-lg-4 h-100 mb-4" id=${taskData.id}" style="height:200px">
     <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success"><i class="fas fa-edit"></i></button>
@@ -45,24 +69,13 @@ const saveChanges = () =>
   `;
 
   //Adding a new card ajdacent to the previous card
-  taskContainer.insertAdjacentHTML('beforeend', newCard);
+  taskContainer.insertAdjacentHTML('beforeend', generateNewCard(newCard));
 
   //Pusing the new task object into a global store array
   globalStore.push(taskData);
 
   //Adding the Global Store array into the Local Storage
   localStorage.setItem("tasky",JSON.stringify({cards : globalStore}));
-
-
-
-
-
-
-
-
-
-
-
 
 };
 
