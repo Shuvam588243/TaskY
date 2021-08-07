66c1b07819a458c589753a2b93d6fd17c8a71b37
shuvam Sengupta <36786690+Shuvam588243@users.noreply.github.com>
2021-08-07 06:38:01 +0000

    New Changes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+// card = document.getElementsByClassName('card');
+// card.addEventListener('hover',()=>
+// {
+//   card.classList.add('animate__headShake');
+// });
+
 //fetching the container where card will be added
 const taskContainer = document.querySelector('.task_container');
 //Initializing a Empty Global store array
@@ -6,9 +12,9 @@ const globalStore = [];
 
 
 const generateNewCard = (taskData) =>`<div class="col-sm-12 col-md-6 col-lg-4 h-100 mb-4" id=${taskData.id} style="height:200px">
-    <div class="card">
+    <div class="card animated animate__headShake">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success"><i class="fas fa-edit"></i></button>
+    <button type="button" class="btn btn-outline-info"><i class="fas fa-edit"></i></button>
     <button type="button" class="btn btn-outline-danger" onclick="taskDelete()"><i class="fas fa-trash-alt"></i></button>
   </div>
   <div class="card-body">
@@ -51,13 +57,18 @@ const saveChanges = () =>
 
 
   //Adding a new card ajdacent to the previous card
-  taskContainer.insertAdjacentHTML('beforeend', generateNewCard(taskData));
+  taskContainer.insertAdjacentHTML('afterbegin', generateNewCard(taskData));
 
   //Pusing the new task object into a global store array
   globalStore.push(taskData);
 
   //Adding the Global Store array into the Local Storage
   localStorage.setItem("tasky",JSON.stringify({cards : globalStore}));
+
+  document.getElementById('imgurl').value = ""
+  document.getElementById('title').value = ""
+  document.getElementById('type').value = ""
+  document.getElementById('description').value = ""
 
 };
 
